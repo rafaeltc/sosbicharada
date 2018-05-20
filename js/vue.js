5e8723b8357a945372b1a21be9c016c5caf59f47
Jose Rafael <valkyre7@hotmail.com>
2018-05-20 17:33:18 +0100

    updated vue app

--- js/vue.js
+++ js/vue.js
@@ -1,10 +1,26 @@
 // Define a new component called todo-item
+// Vue.component('cat-card', {
+//     // The cat-card component now accepts a
+//     // "prop", which is like a custom attribute.
+//     // This prop is called cat.
+//     props: ['cat'],
+//     template: '<div class="card mb-4 box-shadow"><div class="card-header"><h4 class="my-0 font-weight-normal">{{cat.name}}</h4></div><div class="card-body"><p>{{cat.description}}</p><button  v-if="!cat.sponsored" type="button" class="btn btn-lg btn-block btn-outline-primary">Apadrinhar</button><span v-if="cat.sponsored">Apadrinhado por: <b>{{cat.sponsor}}</b></span></div></div></div>'
+// });
+
 Vue.component('cat-card', {
     // The cat-card component now accepts a
     // "prop", which is like a custom attribute.
     // This prop is called cat.
+    template:"#cat-card",
     props: ['cat'],
-    template: '<div class="card mb-4 box-shadow"><div class="card-header"><h4 class="my-0 font-weight-normal">{{cat.name}}</h4></div><div class="card-body"><p>{{cat.description}}</p><button  v-if="!cat.sponsored" type="button" class="btn btn-lg btn-block btn-outline-primary">Apadrinhar</button><span v-if="cat.sponsored">Apadrinhado por: <b>{{cat.sponsor}}</b></span></div></div></div>'
+    data: function() {
+        return {};
+    },
+    methods: {
+        sponsor: function(name) {
+            alert("you are sponsoring " + this.cat.name + "! That's so great!");
+        }
+    }
 });
 
 var cats = [
@@ -33,6 +49,7 @@ var vm = new Vue({
     data: {
         cats: cats,
         tab: 'main'
-    }
+    },
+    methods: {}
 });
 
